861b62f40fbe06b026eb2e8500569ceb4d477f87
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-01-12 02:01:32 -0600

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <draggable 
+      :list="list">
+      <div 
+        v-for="element in list"
+        :key="element.name"
+      >
+        {{element.name}}
+      </div>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'HelloWorld',
+  components: {
+    draggable
+  },
+  data () {
+    return {
+      list: [
+        {name:"John"}, 
+        {name:"Joao"}, 
+        {name:"Jean"} 
+      ]
+    }
+	},
+  props: {
+    msg: String
+  },
+  methods:{
+    add: function(){
+      this.list.push({name:'Juan'});
+    },
+    replace: function(){
+      this.list=[{name:'Edgard'}]
+    }
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
