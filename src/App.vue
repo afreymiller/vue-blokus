897af41fc4a1ca92f2fa17c9e299e70d741e568d
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-01-12 02:09:42 -0600

    getting rid of some boilerplate code from vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
+    <!--Leave as example for tying in images-->
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Board msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
