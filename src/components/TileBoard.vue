23ce08c39583b827e978df43a9ece5e4ef64459c
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-03-08 11:49:23 -0600

    adding tileboard component

---
+++ src/components/TileBoard.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <canvas id="board"></canvas>
+</template>
+
+<script>
+var $ = require('jquery')
+window.jQuery = $
+
+export default {
+  name: 'TileBoard',
+  props: {
+    filled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+canvas {
+    background: #fff;
+    margin: 20px;
+}
+</style>
