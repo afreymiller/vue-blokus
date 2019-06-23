c2749307879ff9341faf86010863e4797f8a1a61
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-06-23 14:18:14 -0500

    adding collision detection

--- src/helpers/canvasApi.js
+++ src/helpers/canvasApi.js
@@ -70,8 +70,28 @@ const isValid = (gameConfig, tileConfig, xOffset, yOffset) => {
   let xBox = getCoord(xOffset);
   let yBox = getCoord(yOffset);
 
+  let i, j, xTile, yTile;
+
   console.log(xBox);
   console.log(yBox);
+
+  for (i = 0; i < 5; i++) {
+    for (j = 0; j < 5; j++) {
+      if (tileConfig[i][j] === 1) {
+        xTile = 2 - i;
+        yTile = 2 - j;
+
+        if ((xTile + xBox <= 20) && (xTile + xBox >= 0) &&
+          (yTile + yBox <= 20) && (yTile + yBox >= 0)) {
+            if (gameConfig[xTile+xBox][yTile+yBox] === 1) {
+              return false;
+            }
+          }        
+      }
+    }
+  }
+
+  return true;
 }
 
 const canvasApi = {
@@ -100,7 +120,7 @@ const canvasApi = {
       }
     }
 
-    isValid(gameConfig, tileConfig, xOffSet, yOffset);
+    console.log(isValid(gameConfig, tileConfig, xOffSet, yOffset));
 
     renderPolyominoTiles(currState, ctx);
 
