a14f12312d8880814647383adf039b3685d4bf1e
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-06-23 13:02:09 -0500

    updating name config

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -23,11 +23,11 @@ export default {
     return {
       left: 0,
       top: 0,
-      config: [
+      tileConfig: [
         [0, 0, 0, 0, 0],
         [0, 1, 0, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0]
       ]
     }
@@ -45,13 +45,13 @@ export default {
       this.left = e.pageX - offset.left;
       this.top = e.pageY - offset.top;
 
-      canvasApi.updateCanvas(this.config, this.left, this.top);
+      canvasApi.updateCanvas(this.tileConfig, this.left, this.top);
     }
   },
   mounted() {
     window.addEventListener('mousemove', this.calculatePosition);
     window.addEventListener('mouseup', this.calculatePosition);
-    canvasApi.updateCanvas(this.config, 0, 0);
+    canvasApi.updateCanvas(this.tileConfig, 0, 0);
   }
 }
 </script>
