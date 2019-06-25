c0a1c216ac0666f10e5bf35ea8cf0997e57527a6
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-06-24 22:46:32 -0500

    adding a basic modicum of unit tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-undef */
 let assert = require('assert');
-// import canvasApi from '../src/helpers/canvasApi.js';
+let Set = require('./Set.js');
 
 describe('Array', function() {
   describe('indexOf()', function() {
     it('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+describe('Set', function() {
+  describe('isEmpty()', function() {
+    it('should return true if set is empty', () => {
+      let s = new Set();
+      assert.equal(s.isEmpty(), true);
+    })
   });
 });
