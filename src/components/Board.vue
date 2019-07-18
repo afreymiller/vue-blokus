3c45787f7f923df82d3d7f42ce708ec91a9ecc98
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2019-07-18 00:12:56 -0500

    getting rid of more stuff

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,18 +2,12 @@
 <template>
   <div>
   <canvas id="board"></canvas>
-  {{count}}
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
-  name: 'Board',
- 
-  computed: mapState({
-    count: state => state.game.count
-  })
+  name: 'Board'
 }
 </script>
 
